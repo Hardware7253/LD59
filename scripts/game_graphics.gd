d0dbf6db28d064d69c85994fd23ab382a9d3e4c8
oxnh <77563973+Hardware7253@users.noreply.github.com>
2026-04-20 21:27:26 +1200

    Updated graphics
    Gave the UI and game nicer colours

--- scripts/game_graphics.gd
+++ scripts/game_graphics.gd
@@ -1,27 +1,27 @@
 extends Node
 
-const BUILDING_COLOR := Color("#252525")
-const BUILDING_INPUT_COLOR := Color("#a8661a")
-const BUILDING_OUTPUT_COLOR := Color("#2691b1")
+const BUILDING_COLOR := Color("#30343e")
+const BUILDING_INPUT_COLOR := Color("#cfb63c")
+const BUILDING_OUTPUT_COLOR := Color("#37579c")
 
-const WIRE_COLOR := Color("#54a83b")
+const WIRE_COLOR := Color("#6fe287")
 
 const WAVE_VIEWER_LINE_SIZE := 0.5
 const WAVE_VIEWER_ORIGIN_LINE_SIZE := 0.15
-const GOAL_LINE_COLOR := Color("#74c2c2")
-const GEN_LINE_COLOR := Color("#c274c2")
-const OSCILLOSCOPE_LINE_COLOR := Color("#74c274")
-const ORIGIN_LINE_COLOR := Color("#ffffff")
+const GOAL_LINE_COLOR := Color("#65bdce")
+const GEN_LINE_COLOR := Color("#a06fe2")
+const OSCILLOSCOPE_LINE_COLOR := Color("#6fe287")
+const ORIGIN_LINE_COLOR := Color("#dde4e5")
 
 const RESET_MODULATION_COLOR := Color("#ffffff")
 const GHOST_MODULATION_COLOR := Color("ffffff99")
 const GHOST_ERROR_MODULATION_COLOR := Color("ff404099")
 
-const GAME_BG_COLOR := Color("#202020")
+const GAME_BG_COLOR := Color("#292c36")
 
 # For the in-game message board
-const ERROR_MSG_FONT_COLOR := Color("#c27474")
-const MSG_FONT_COLOR := Color("#ffffff")
+const ERROR_MSG_FONT_COLOR := Color("#e27b6f")
+const MSG_FONT_COLOR := Color("#dde4e5")
 
 
 func _ready() -> void:
